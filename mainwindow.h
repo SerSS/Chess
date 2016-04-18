984a31435ec911d6ffbd5aa32ac6f119e5414a57
MrHappyyy <v.yur4ik@gmail.com>
2016-04-18 15:23:41 +0300

    merge

--- mainwindow.h
+++ mainwindow.h
@@ -25,10 +25,6 @@ const int cellHeight = 50;
 
 //int figuresOnBoard[8][8];
 //int playerOnBoard[8][8];
-<<<<<<< HEAD
-=======
-
->>>>>>> Sima
 
 class GUI : public QObject  {
     Q_OBJECT
@@ -40,10 +36,8 @@ public:
 
     int figuresOnBoard[8][8];
     int playerOnBoard[8][8];
-<<<<<<< HEAD
-=======
+
     int startFilling[8] = { 1, 2, 3, 4, 5, 3, 2, 1};
->>>>>>> Sima
 
     void createGUI();
     void checkThePossibilityOfStep(int x, int y, int checkableX, int checkableY);
