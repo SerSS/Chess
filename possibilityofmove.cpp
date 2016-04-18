49ad2837a055a906aa0851f18ec6298052967002
MrHappyyy <v.yur4ik@gmail.com>
2016-04-18 15:02:12 +0300

    добавлена проверка на возможность хода белой пешки

--- possibilityofmove.cpp
+++ possibilityofmove.cpp
@@ -1,6 +1,29 @@
 #include "mainwindow.h"
 
-
 void GUI::checkThePossibilityOfStep(int x, int y, int checkableX, int checkableY){
 
+    switch (figuresOnBoard[x][y]) {
+        case 1://ладья
+
+            break;
+        case 2://лошадь
+
+            break;
+        case 3://офицер
+
+            break;
+        case 4://дамка
+
+            break;
+        case 5://король
+
+            break;
+        case 8://пешка
+            if (playerOnBoard[x][y] == 1) {
+
+            } else if (playerOnBoard[x][y] == 2) {
+
+            }
+            break;
+    }
 }
