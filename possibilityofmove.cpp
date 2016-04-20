0d9b4b8f035e81b6556fc4dc2a1ed9afda7fb262
MrHappyyy <v.yur4ik@gmail.com>
2016-04-20 15:50:10 +0300

    передеана пешка

--- possibilityofmove.cpp
+++ possibilityofmove.cpp
@@ -13,14 +13,14 @@ bool GUI::checkThePossibilityOfStep(int x, int y, int checkableX, int checkableY
                 return false;
             }*/
             if (x == checkableX) {
-                if (checkableY > y) {
-                    for (int i = y + 1; i < checkableY; i++) {
+                if (checkableY > y) {//вправо
+                    for (int i = y + 1; i < checkableY; i++) {//проверка нет ли по пути фигруы
                         if (figuresOnBoard[x][i] != 0) {
                             return false;
                         }
                     }
-                } else if (checkableY < y) {
-                    for (int i = y - 1; i > checkableY; i--) {
+                } else if (checkableY < y) {//влево
+                    for (int i = y - 1; i > checkableY; i--) {//проверка нет ли по пути фигруы
                         if (figuresOnBoard[x][i] != 0) {
                             return false;
                         }
@@ -29,14 +29,14 @@ bool GUI::checkThePossibilityOfStep(int x, int y, int checkableX, int checkableY
                     return false;
                 }
             } else if (y == checkableY) {
-                if (checkableX > x) {
-                    for (int i = x + 1; i < checkableX; i++) {
+                if (checkableX > x) {//вниз
+                    for (int i = x + 1; i < checkableX; i++) {//проверка нет ли по пути фигруы
                         if (figuresOnBoard[i][y] != 0) {
                             return false;
                         }
                     }
-                } else if (checkableX < x) {
-                    for (int i = x - 1; i > checkableX; i--) {
+                } else if (checkableX < x) {//вверх
+                    for (int i = x - 1; i > checkableX; i--) {//проверка нет ли по пути фигруы
                         if (figuresOnBoard[i][y] != 0) {
                             return false;
                         }
@@ -171,7 +171,77 @@ bool GUI::checkThePossibilityOfStep(int x, int y, int checkableX, int checkableY
             }
             break;
         case 6://пешка
-
+            if (playerOnBoard[x][y] == 1) {//белые
+                if (checkableX > x || checkableY > (y+1) || checkableY < (y-1)) {
+                    return false;
+                }
+                if (checkableY == y) {
+                    if (x == 6) {
+                        if (checkableX >= (x-2)) {
+                            for (int i = 5; i >= checkableX; i--) {
+                                if (figuresOnBoard[i][y] != 0) {
+                                    return false;
+                                }
+                            }
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        if (checkableX == (x-1)) {
+                            if (figuresOnBoard[checkableX][checkableY] != 0) {
+                                return false;
+                            }
+                        } else {
+                            return false;
+                        }
+                    }
+                } else if ((checkableX == (x-1)) && (checkableY == (y+1) || checkableY == (y-1))){
+                    if (playerOnBoard[checkableX][checkableY] != 2) {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+                if (checkableX == 0) {
+                    exchangeOfAPawn(x, y);
+                }
+            } else if (playerOnBoard[x][y] == 2) {//черные
+                if (checkableX < x || checkableY > (y+1) || checkableY < (y-1)) {
+                    return false;
+                }
+                if (checkableY == y) {
+                    if (x == 1) {
+                        if (checkableX <= (x+2)) {
+                            for (int i = 2; i <= checkableX; i++) {
+                                if (figuresOnBoard[i][y] != 0) {
+                                    return false;
+                                }
+                            }
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        if (checkableX == (x+1)) {
+                            if (figuresOnBoard[checkableX][checkableY] != 0) {
+                                return false;
+                            }
+                        } else {
+                            return false;
+                        }
+                    }
+                } else if ((checkableX == (x+1)) && (checkableY == (y+1) || checkableY == (y-1))){
+                    if (playerOnBoard[checkableX][checkableY] != 1) {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+                if (checkableX == 7) {
+                    exchangeOfAPawn(x, y);
+                }
+            } else {
+                return false;
+            }
             break;
     }
     moveOneStep(x, y, checkableX, checkableY);
