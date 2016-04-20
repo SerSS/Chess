e94cbbfe27f61d1588398fa6e1aba86e524a6a32
MrHappyyy <v.yur4ik@gmail.com>
2016-04-20 13:04:01 +0300

    исправлена ладья

--- possibilityofmove.cpp
+++ possibilityofmove.cpp
@@ -2,89 +2,55 @@
 
 void GUI::checkThePossibilityOfStep(int x, int y, int checkableX, int checkableY){
     //проверка попытки боя своей фигуры
-    if (playerOnBoard[x][y] == 0 || playerOnBoard[x][y] == playerOnBoard[checkableX][checkableY] || (x==checkableX && y==checkableY)) {
+    if (x < 0 || y < 0 || x > 7 || y > 7 || checkableX < 0 || checkableY < 0 || checkableX > 7 || checkableY > 7 || //проверка выхода за розмерность доски
+        playerOnBoard[x][y] == 0 || playerOnBoard[x][y] == playerOnBoard[checkableX][checkableY] || (x==checkableX && y==checkableY)) {
         click =0;
-        return;
+        return false;
     }
 
     switch (figuresOnBoard[x][y]) {
         case 1://ладья
-            if (checkableX < 8 && checkableY < 8 && checkableX >= 0 && checkableY >= 0) {
-                if (checkableX == x) {
-                    if (checkableY < y) {//влево
-                        for (int i = y - 1; i > checkableY; i--) {//проверка нет ли по пути фигур
-                            if (figuresOnBoard[x][i] != 0) {
-                                return;
-                            }
+            /*if (x != checkableX && y != checkableY) {
+                return false;
+            }*/
+            if (x == checkableX) {
+                if (checkableY > y) {
+                    for (int i = y; y < checkableY; i++) {
+                        if (figuresOnBoard[x][i] != 0) {
+                            return false;
                         }
-                        //реализация хода
-                        moveOneStep(x, y, checkableX, checkableY);
-                    } else if (checkableY > y) {//вправо
-                        for (int i = y + 1; i < checkableY; i++) {//проверка нет ли по пути фигур
-                            if (figuresOnBoard[x][i] != 0) {
-                                return;
-                            }
-                        }
-                        //реализация хода
-                        moveOneStep(x, y, checkableX, checkableY);
                     }
-                } else if (checkableY == y) {
-                    if (checkableX < x) {//вверх
-                        for (int i = x - 1; i > checkableX; i--) {//проверка нет ли по пути фигур
-                            if (figuresOnBoard[i][y] != 0) {
-                                return;
-                            }
+                } else if (checkableY < y) {
+                    for (int i = y; y > checkableY; i--) {
+                        if (figuresOnBoard[x][i] != 0) {
+                            return false;
                         }
-                        //реализация хода
-                        moveOneStep(x, y, checkableX, checkableY);
-                    } else if (checkableX > x) {//вниз
-                        for (int i = x + 1; i < checkableX; i++) {//проверка нет ли по пути фигур
-                            if (figuresOnBoard[i][y] != 0) {
-                                return;
-                            }
-                        }
-                        //реализация хода
-                        moveOneStep(x, y, checkableX, checkableY);
                     }
+                } else {
+                    return false;
                 }
+            } else if (y == checkableY) {
+                if (checkableX > x) {
+                    for (int i = x; x < checkableX; i++) {
+                        if (figuresOnBoard[x][i] != 0) {
+                            return false;
+                        }
+                    }
+                } else if (checkableX < x) {
+                    for (int i = x; x > checkableX; i--) {
+                        if (figuresOnBoard[x][i] != 0) {
+                            return false;
+                        }
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
             }
             break;
         case 2://лошадь
-            if (checkableX < 8 && checkableY < 8 && checkableX >= 0 && checkableY >= 0) {
-                if (checkableX == (x-2)) {//вверх
-                    if (checkableY == (y-1)) {//влево
-                        //реализация хода
-                        moveOneStep(x, y, checkableX, checkableY);
-                    } else if (checkableY == (y+1)) {//вправо
-                        //реализация хода
-                        moveOneStep(x, y, checkableX, checkableY);
-                    }
-                } else if (checkableX == (x+2)) {//вниз
-                    if (checkableY == (y-1)) {//влево
-                        //реализация хода
-                        moveOneStep(x, y, checkableX, checkableY);
-                    } else if (checkableY == (y+1)) {//вправо
-                        //реализация хода
-                        moveOneStep(x, y, checkableX, checkableY);
-                    }
-                } else if (checkableY == (y-2)) {//влево
-                    if (checkableX == (x-1)) {//вверх
-                        //реализация хода
-                        moveOneStep(x, y, checkableX, checkableY);
-                    } else if (checkableX == (x+1)) {//вниз
-                        //реализация хода
-                        moveOneStep(x, y, checkableX, checkableY);
-                    }
-                } else if (checkableY == (y+2)) {//вправо
-                    if (checkableX == (x-1)) {//вверх
-                        //реализация хода
-                        moveOneStep(x, y, checkableX, checkableY);
-                    } else if (checkableX == (x+1)) {//вниз
-                        //реализация хода
-                        moveOneStep(x, y, checkableX, checkableY);
-                    }
-                }
-            }
+
             break;
         case 3://офицер
             if (checkableX < 8 && checkableY < 8 && checkableX >= 0 && checkableY >= 0) {
@@ -93,28 +59,28 @@ void GUI::checkThePossibilityOfStep(int x, int y, int checkableX, int checkableY
                     if(x < checkableX && y < checkableY){// вниз вправо
                         for(int i=x, j=y; i < checkableX; i++, j++){
                             if(figuresOnBoard[i][j] != 0){
-                                return;
+                                return false;
                             }
                         }
                     }
                     if(x < checkableX && y > checkableY){// вниз влево
                         for(int i=x, j=y; i < checkableX; i++, j--){
                             if(figuresOnBoard[i][j] != 0){
-                                return;
+                                return false;
                             }
                         }
                     }
                     if(x > checkableX && y < checkableY){// вверх вправо
                         for(int i=x, j=y; i > checkableX; i--, j++){
                             if(figuresOnBoard[i][j] != 0){
-                                return;
+                                return false;
                             }
                         }
                     }
                     if(x > checkableX && y > checkableY){// вверх влево
                         for(int i=x, j=y; i > checkableX; i--, j--){
                             if(figuresOnBoard[i][j] != 0){
-                                return;
+                                return false;
                             }
                         }
                     }
@@ -206,79 +172,11 @@ void GUI::checkThePossibilityOfStep(int x, int y, int checkableX, int checkableY
             }
             break;
         case 6://пешка
-            if (playerOnBoard[x][y] == 1) {//белые
-                if (checkableX < x) {//не пытаются ли пойти назад
-                    if (y == checkableY &&      //проверка ход или бой
-                            playerOnBoard[checkableX][checkableY] == 0) {
-                        if (playerOnBoard[x][y] == 6) {//прверка первого хода
-                            if (checkableX >= 4 && figuresOnBoard[x-1][y] == 0) {//нет ли по пути фигуры
-                                //реализация первго хода
-                                moveOneStep(x, y, checkableX, checkableY);
-                                if (checkableX == 0) {
-                                    //замена пешки
-                                    exchangeOfAPawn(checkableX, checkableY);
-                                }
-                            }
-                        } else {
-                            if (checkableX == (x-1)) {
-                                //реализация остальных ходов
-                                moveOneStep(x, y, checkableX, checkableY);
-                                if (checkableX == 0) {
-                                    //замена пешки
-                                    exchangeOfAPawn(checkableX, checkableY);
-                                }
-                            }
-                        }
 
-                    } else if ((checkableY == (y-1) || checkableY == (y+1)) &&
-                               checkableX == (x-1) &&           //проверка боя
-                               playerOnBoard[checkableX][checkableY] == 2) {
-                        //реализация боя
-                        moveOneStep(x, y, checkableX, checkableY);
-                        if (checkableX == 0) {
-                            //замена пешки
-                            exchangeOfAPawn(checkableX, checkableY);
-                        }
-                    }
-                }
-            } else if (playerOnBoard[x][y] == 2) {//черные
-                if (checkableX > x) {//не пытаются ли пойти назад
-                    if (y == checkableY &&      //проверка ход или бой
-                            playerOnBoard[checkableX][checkableY] == 0) {
-                        if (playerOnBoard[x][y] == 1) {//прверка первого хода
-                            if (checkableX <= 3 && figuresOnBoard[x+1][y]) {//нет ли по пути фигуры
-                                //реализация первго хода
-                                moveOneStep(x, y, checkableX, checkableY);
-                                if (checkableX == 7) {
-                                    //замена пеешки
-                                    exchangeOfAPawn(checkableX, checkableY);
-                                }
-                            }
-                        } else {
-                            if (checkableX == (x+1)) {
-                                //реализация остальных ходов
-                                moveOneStep(x, y, checkableX, checkableY);
-                                if (checkableX == 7) {
-                                    //замена пеешки
-                                    exchangeOfAPawn(checkableX, checkableY);
-                                }
-                            }
-                        }
-
-                    } else if ((checkableY == (y-1) || checkableY == (y+1)) &&
-                               checkableX == (x+1) &&           //проверка боя
-                               playerOnBoard[checkableX][checkableY] == 1) {
-                        //реализация боя
-                        moveOneStep(x, y, checkableX, checkableY);
-                        if (checkableX == 7) {
-                            //замена пеешки
-                            exchangeOfAPawn(checkableX, checkableY);
-                        }
-                    }
-                }
-            }
             break;
     }
+    moveOneStep(x, y, checkableX, checkableY);
+    return true;
 }
 
 void GUI::moveOneStep(int x, int y, int checkableX, int checkableY) {   //Делает ход (перемещает фигуру)
